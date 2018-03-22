efc0e420e587bba0737027df9d50bf711fd74f44
wyh19 <wyh_19@163.com>
2018-03-22 19:41:09 +0800

    添加dashboard

--- common-system/src/page/dashboard/frame.js
+++ common-system/src/page/dashboard/frame.js
@@ -2,10 +2,10 @@
  * Created by 30113 on 2018/3/22.
  */
 import React from 'react'
-import { Layout,  Icon } from 'antd';
+import {Layout, Icon} from 'antd';
 import MenuBar from './menubar'
 
-const { Header, Sider, Content } = Layout;
+const {Header, Sider, Content} = Layout;
 
 class Dashboard extends React.Component {
     state = {
@@ -19,26 +19,27 @@ class Dashboard extends React.Component {
             mode: collapsed ? 'vertical' : 'inline',
         });
     }
+
     render() {
         return (
-            <Layout  >
+            <Layout>
                 <Sider
                     trigger={null}
                     collapsible
                     collapsed={this.state.collapsed}
                 >
-                    <div className="logo" />
+                    <div className="logo"/>
                     <MenuBar mode={this.state.mode}/>
                 </Sider>
                 <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
+                    <Header style={{background: '#fff', padding: 0}}>
                         <Icon
                             className="trigger"
                             type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
                             onClick={this.toggle}
                         />
                     </Header>
-                    <Content style={{  padding: 24, background: '#fff', minHeight: 280 }}>
+                    <Content style={{padding: 24, background: '#fff', minHeight: 280}}>
                         {this.props.children}
                     </Content>
                 </Layout>

f239b891beffb5c388f41951e22ece2a1779f414
wyh19 <wyh_19@163.com>
2018-03-26 16:31:35 +0800

    添加my-redux项目

---
+++ my-redux/src/App.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 30113 on 2018/3/26.
+ */
+import React from 'react'
+
+class App extends React.Component{
+
+    render(){
+        return (
+            <div>
+                app
+            </div>
+        )
+    }
+}
+export default App

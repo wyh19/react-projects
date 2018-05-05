6badbfdd50fd048c7f3558a55c2d6affa1578ec7
wyh19 <wyh_19@163.com>
2018-05-05 23:20:27 +0800

    开发json-editor

--- json-editor/webpack.config.js
+++ json-editor/webpack.config.js
@@ -1,12 +1,18 @@
 /**
  * Created by 30113 on 2018/5/4.
  */
+const webpack = require('webpack')
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     mode: 'development',
     entry: {
-        app: path.join(__dirname, './src/index.js')
+        //app: path.join(__dirname, './src/index.js')
+        app:[
+            'react-hot-loader/patch',
+            path.join(__dirname, './src/index.js')
+        ]
     },
     output: {
         path: path.join(__dirname, './dist'),
@@ -23,11 +29,43 @@ module.exports = {
                         loader: 'babel-loader',
                         options: {
                             presets: [["env"], 'react'],
+                            plugins:["react-hot-loader/babel"]
                         }
                     }
                 ]
-
-            }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
         ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.join(__dirname, './public/index.html')
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve:{
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "app")
+        ],
+        extensions: [".js", ".json", ".jsx", ".css"],
+    },
+    devServer:{
+        host:'0.0.0.0',
+        port:'8888',
+        contentBase:path.join(__dirname, './dist'),
+        hot:true,
+        overlay:{
+            errors:true
+        }
     }
 }
